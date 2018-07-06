a3dae7af10c3e5b06173a01fa53d0ed0f2197c3d
nklein16 <nathaniel.klein@outlook.com>
2018-07-06 01:57:32 -0400

    commit jQueryExercise files and changes to .gitignore

---
+++ jQuery/exercise.js
@@ -0,0 +1,19 @@
+// Correctly include jQuery
+if(jQuery){
+    alert("jQuery loaded");
+}
+else {
+    alert("no jQuery");
+}
+
+// Select all dics and give them a purple background
+$("div").css("background", "purple");
+
+// Select all the dics with class "highlight" and make them 200px wide
+$(".highlight").css("width", "200px");
+
+// Selet the div with id "third" and give it an orange border
+$("#third").css("border", "4px solid orange");
+
+// Bonus: Select the first div only and change its font color to pink
+$("div:first-of-type").css("color", "pink");
